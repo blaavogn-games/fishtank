2d73551c0d87ad119eecf86f75a91c8294bb5763
Unknown <themunk@live.dk>
2017-03-02 10:44:05 +0100

    implementing basic sound

---
+++ Assets/ressources/player/PlayerSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSound : MonoBehaviour {
+    [Header("Sound")]
+    public AudioClip[] SwimClips;
+    public AudioClip SwimAmbient;
+    public AudioClip Ambient;
+    private AudioSource[] audioSources;
+    private AudioLowPassFilter filter;
+    
+    void Start ()
+    {
+        audioSources = GetComponents<AudioSource>();
+        audioSources[0].clip = SwimAmbient;
+        audioSources[0].Play();
+        audioSources[1].clip = Ambient;
+        audioSources[1].Play();
+        filter = GetComponent<AudioLowPassFilter>();
+        
+    }
+
+    public void SetSpeed(float speed)
+    {
+        audioSources[0].volume = speed;
+        //filter.cutoffFrequency = 100+speed*25;
+    }
+}
