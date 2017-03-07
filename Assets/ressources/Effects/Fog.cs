cbffc2a336ea73b23eb75f9e78255d52f7cdd133
Holger Borum <blaa.vogn@gmail.com>
2017-03-07 10:19:49 +0100

    Alpha version

--- Assets/ressources/Effects/Fog.cs
+++ Assets/ressources/Effects/Fog.cs
@@ -5,16 +5,14 @@ public class Fog : MonoBehaviour {
     public float FogIncreasePerSecond = 0.01F;
     public float MaxFogLevel = 0.025f;
     public Color FogColor;
-	// Use this for initialization
-	void Start () {
+    void Start () {
         RenderSettings.fogDensity = 0.0F;
         RenderSettings.fogColor = FogColor;
         RenderSettings.fogMode = FogMode.ExponentialSquared;
         RenderSettings.fog = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update () {
         if (RenderSettings.fogDensity <= MaxFogLevel)
             RenderSettings.fogDensity += FogIncreasePerSecond * Time.deltaTime;
         else
--- Assets/ressources/player/Player.cs
+++ Assets/ressources/player/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour {
     private Rigidbody _rigidbody;
     //private AudioLowPassFilter _audioLowPassFilter;
     public int InvertedAxis = 1;
+    private bool dashDown = false;
     private PlayerSound playerSound;
 
     [Header("Swimming Controls")]
@@ -15,6 +16,7 @@ public class Player : MonoBehaviour {
     public float Acceleration = 4000;
     public float HRotation = 120;
     public float VRotation = 50;
+    public float DashSpeed = 30;
 
     private float dashTimer = 0;
     [Tooltip("In Seconds")]
@@ -38,6 +40,7 @@ public class Player : MonoBehaviour {
     Quaternion originalRotation;
 
 
+
     //[Header("Snap Rotation Transition")]
     //[Tooltip("In Seconds")]
     //public float TransitionSpeed = 0.3f;
@@ -58,98 +61,50 @@ public class Player : MonoBehaviour {
 
     void Update ()
     {
-        //GetComponent<AudioLowPassFilter>().cutoffFrequency = 100 + _rigidbody.velocity.magnitude*25;
-
-
-        wiggle.wiggleSpeed = Mathf.Max(5,_rigidbody.velocity.magnitude);
         //Debug.Log(_rigidbody.velocity.magnitude);
         switch (state)
         {
             case State.SWIM:
-                int hDir = 0;
-                int vDir = 0;
+                float hDir = 0.0f, vDir = 0.0f, constAcc = 0.0f, boost = 0;
 
+                //Forward movement
                 //Swims if possible. Increments dash timer.
-                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Joystick1Button0))
+                if (Mathf.Max(Input.GetAxis("Forward"), Input.GetAxis("ForwardAlt"))> 0)
                 {
-                    if (MaxSwimVelocity > _rigidbody.velocity.magnitude && swimCooldownTimer <= 0)
-                        Swim();
+                    if (MaxSwimVelocity > _rigidbody.velocity.magnitude)
+                        constAcc = 1;
+                    if (_rigidbody.velocity.magnitude < 7)
+                        boost = 900;
                     dashTimer += Time.deltaTime;
                 }
-
                 //Dashes if timer within threshhold.
-                if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Joystick1Button0))
+                if (Input.GetButton("Dash") && !dashDown)
                 {
+                    dashDown = true;
                     if (dashTimer < DashThreshold && dashCooldownTimer <= 0)
                     {
-                        Dash();
+                        boost = DashSpeed;
                         dashCooldownTimer = DashCooldown;
                         swimCooldownTimer = DashCooldown * 2;
                     }
                     dashTimer = 0;
                 }
-                    //Turning
-                    if (Input.GetAxis("Vertical") > 0)
-                        vDir += 1 * InvertedAxis;
-                    if (Input.GetAxis("Vertical") < 0)
-                        vDir -= 1 * InvertedAxis;
-                    if (Input.GetAxis("Horizontal") < 0)
-                        hDir -= 1;
-                if (Input.GetAxis("Horizontal") > 0)
-                    hDir += 1;
-                    
-		        if (Input.GetKey(KeyCode.P))
-                        InvertedAxis *= -1;
-                    if (Input.GetKey(KeyCode.R))
-                    {
-                        Scene scene = SceneManager.GetActiveScene();
-                        SceneManager.LoadScene(scene.name);
-                    }
+                else if(!Input.GetButton("Dash"))
+                {
+                    dashDown = false;
+                }
+                float accAdded = boost + (constAcc * Acceleration * Time.deltaTime);
+                _rigidbody.AddForce(accAdded * transform.forward);
+                wiggle.wiggleSpeed = Mathf.Max(5,_rigidbody.velocity.magnitude + accAdded * 0.01f);
 
-                //Angle flips 90 and 180
-                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Joystick1Button5))
-                {
-                    if (MouseLookEnabled)
-                        transform.rotation = originalRotation;
-                    //originalRotation = transform.rotation;
-                    state = State.LOOKTRANSITION;
-                    //transitionTimer = 0;
-                    //targetRotation = Quaternion.AngleAxis(90, Vector3.up);
-                    //rotationGoal = 90;
-                    LookTransition(new Vector3(0, 90, 0), Space.World);
-                    originalRotation = transform.localRotation;
-                }
-                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Joystick1Button4))
-                {
-                    if (MouseLookEnabled)
-                        transform.rotation = originalRotation;
-                    //originalRotation = transform.rotation;
-                    state = State.LOOKTRANSITION;
-                    //transitionTimer = 0;
-                    //targetRotation = Quaternion.AngleAxis(-90, Vector3.up);
-                    //rotationGoal = -90;
-                    LookTransition(new Vector3(0, -90, 0), Space.World);
-                    originalRotation = transform.localRotation;
-                }
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    if(MouseLookEnabled)
-                        transform.rotation = originalRotation;
-                    //originalRotation = transform.rotation;
-                    //targetRotation = Quaternion.AngleAxis(180, Vector3.up);
-                    state = State.LOOKTRANSITION;
-                    //transitionTimer = 0;
-                    //rotationGoal = 180;
-                    LookTransition(new Vector3(0, 180, 0), Space.World);
-                    originalRotation = transform.localRotation;
-                }
+                //Turning
+                vDir += InvertedAxis * Input.GetAxis("Vertical") * VRotation;
+                hDir += HRotation * Input.GetAxis("Horizontal");
 
-                //Apply rotations from turning controls
-                transform.Rotate(new Vector3(0, HRotation * hDir, 0) * Time.deltaTime, Space.World);
-                transform.Rotate(new Vector3(VRotation * vDir, 0, 0) * Time.deltaTime, Space.Self);
-                //Debug.Log(HRotation * hDir * 0.2f);
-                wiggle.baseRotation.x = VRotation * vDir * 0.16f;
-                wiggle.baseRotation.y = HRotation * hDir * 0.1f;
+                transform.Rotate(new Vector3(0, hDir, 0) * Time.deltaTime, Space.World);
+                transform.Rotate(new Vector3(vDir, 0, 0) * Time.deltaTime, Space.Self);
+                wiggle.baseRotation.x = vDir * 0.16f;
+                wiggle.baseRotation.y = hDir * 0.1f;
 
                 if (MouseLookEnabled)
                 {
@@ -161,57 +116,43 @@ public class Player : MonoBehaviour {
                     Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
                     transform.rotation = originalRotation * xQuaternion * yQuaternion;
                 }
-
-                //transform.rotation = Quaternion.Euler(Vector3.MoveTowards(transform.rotation, new Vector3(0, transform.rotation.y, 0), 2));
                 break;
-            /*case State.LOOKTRANSITION:
-                transitionTimer += Time.deltaTime/TransitionSpeed;
-                //Quaternion yQuaternion2 = Quaternion.AngleAxis(rotationGoal, Vector3.up);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, transitionTimer);
-                //transform.Rotate(new Vector3(0, rotationGoal / (TransitionSpeed / Time.deltaTime), 0), Space.World);
-                if (transitionTimer >= 1)
-                {
-                    //originalRotation = transform.rotation;
-                    state = State.SWIM;
-                }
-                break;*/
         }
         _rigidbody.velocity = transform.forward.normalized * _rigidbody.velocity.magnitude;
         playerSound.SetSpeed(_rigidbody.velocity.magnitude);
         CoolDown();
-    }
-    private void Dash()
-    {
-        _rigidbody.AddForce(transform.forward * Acceleration*50 * Time.deltaTime);
-    }
-    private void Swim()
-    {
-        _rigidbody.AddForce(transform.forward * Acceleration * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+            SceneManager.LoadScene(0);
+        if (Input.GetKey(KeyCode.Alpha2))
+            SceneManager.LoadScene(1);
+        if (Input.GetKey(KeyCode.Alpha3))
+            SceneManager.LoadScene(2);
+        if (Input.GetKey(KeyCode.Alpha4))
+            SceneManager.LoadScene(3);
+        if (Input.GetKey(KeyCode.P))
+            InvertedAxis *= -1;
+        if (Input.GetKey(KeyCode.R))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
     }
     private void CoolDown()
     {
-        //Count down dash cooldown
         dashCooldownTimer -= Time.deltaTime;
         if (dashCooldownTimer < 0)
             dashCooldownTimer = 0;
-
-        //Count down sim cooldown
         swimCooldownTimer -= Time.deltaTime;
         if (swimCooldownTimer < 0)
             swimCooldownTimer = 0;
     }
     public static float ClampAngle(float angle, float min, float max)
     {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
-    }
-
-    private void LookTransition(Vector3 rotation, Space space)
-    {
-        transform.Rotate(rotation, space);
-        state = State.SWIM;
+        while(angle < -360)
+            angle += 360;
+        while(angle > 360)
+            angle -= 360;
+        return angle;
     }
 }
