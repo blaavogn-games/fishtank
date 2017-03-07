3f8862de805e98e22200b0ca7f511355cc1644ae
Holger Borum <blaa.vogn@gmail.com>
2017-03-07 11:08:14 +0100

    Forgot files

---
+++ Assets/ressources/Foliage/TransparentOnCollide.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentOnCollide : MonoBehaviour {
+    public Color AlbedoNormal, AlbedoTrans;
+    private Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+            mat.SetColor("_Albedo", AlbedoTrans);
+    }
+}
