963f227491cb4aa12470bdb78cd6e62ace3d5a44
Holger Borum <blaa.vogn@gmail.com>
2017-03-16 16:21:39 +0100

    Followers first version

---
+++ Assets/Resources/AI/FollowFish.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowFish : MonoBehaviour {
+    private enum State { FOLLOWING, FOLLOWIDLE, IDLE }
+    private State state = State.IDLE;
+    private Player player;
+    private int followId;
+    public float MinSpeed = 10, MaxSpeed = 30, IdleSpeed = 3;
+    private float[] speedBuffer = new float[15];
+    private int curSpeed = 0, playerSpeed;
+    public Wiggle wiggle;
+    private Vector3 idleTarget;
+
+    void Start () {
+        playerSpeed = speedBuffer.Length - 2;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        idleTarget = RandomTarget(transform.position);
+    }
+
+    void FixedUpdate () {
+        switch(state) {
+            case State.FOLLOWING:
+                speedBuffer[(playerSpeed++) % speedBuffer.Length] = player.GetComponent<Rigidbody>().velocity.magnitude;
+                float speed = Mathf.Max(MinSpeed, speedBuffer[(curSpeed++) % speedBuffer.Length]);
+                speed = Mathf.Min(speed, MaxSpeed);
+                Vector3 target = ChaseTarget(player.transform, followId);
+                if(Move(target, speed)) {
+                    state = State.FOLLOWIDLE;
+                    idleTarget = RandomTarget(player.transform.position);
+                }
+                break;
+            case State.FOLLOWIDLE:
+                if(Move(idleTarget, IdleSpeed))
+                    idleTarget = RandomTarget(player.transform.position);
+                if(Vector3.Distance(transform.position, player.transform.position) > 3)
+                    state = State.FOLLOWING;
+                break;
+            case State.IDLE:
+                if(Move(idleTarget, IdleSpeed))
+                    idleTarget = RandomTarget(transform.position);
+                if(Vector3.Distance(transform.position, player.transform.position) < 5)
+                {
+                    followId = player.GetComponent<PlayerFollowers>().AddFollower(gameObject);
+                    state = State.FOLLOWING;
+                }
+                break;
+        }
+    }
+
+    private Vector3 ChaseTarget(Transform t, int id)
+    {
+        float forward = 0, right = 0, up = 0;
+        switch(id) //Could be more effecient
+        {
+            case 0:
+                forward = -0.5f;
+                right = -2f;
+                up = 0.1f;
+                break;
+            case 1:
+                forward = -0.2f;
+                right = 0.3f;
+                up = 2.1f;
+                break;
+            case 2:
+                forward = -0.7f;
+                right = 1.3f;
+                up = -0.2f;
+                break;
+        }
+
+        return t.position + t.forward * forward + t.right * right + t.up * up;
+    }
+
+    private Vector3 RandomTarget(Vector3 center)
+    {
+        return center + new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f),Random.Range(-2.0f, 2.0f));
+    }
+
+    private bool Move(Vector3 target, float speed)
+    {
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        Vector3 movement = newPosition - transform.position;
+        wiggle.wiggleSpeed = movement.magnitude * 50 + 10;
+        transform.LookAt(newPosition);
+        transform.position = newPosition;
+        return (Vector3.Distance(transform.position, target) < 0.01f);
+    }
+}
---
+++ Assets/Resources/Player/PlayerFollowers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerFollowers : MonoBehaviour {
+    private GameObject[] followers = new GameObject[10];
+
+    public int AddFollower(GameObject g)
+    {
+        for(int i = 0; i < followers.Length; i++) {
+            if(followers[i] == null)
+            {
+                followers[i] = g;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void RemoveFollow(GameObject g)
+    {
+        for(int i = 0; i < followers.Length; i++)
+            if(followers[i] == g)
+                followers[i] = null;
+    }
+}
