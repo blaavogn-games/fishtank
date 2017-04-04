f79efb52268aa2d027a3279c303309fef8424e6e
Holger Borum <blaa.vogn@gmail.com>
2017-04-04 15:46:07 +0200

    FollowFish flees a bit

--- Assets/Resources/Player/CameraController.cs
+++ Assets/Resources/Player/CameraController.cs
@@ -16,14 +16,17 @@ public class CameraController : MonoBehaviour {
         float xMove = Input.GetAxis("LookHor") * Invert;
         float yMove = Input.GetAxis("LookVer") * Invert;
         Vector3 target;
-        if (xMove == 0.0f && yMove == 0.0f)
+        if (Input.GetButton("LookBehind"))
+            target = transform.parent.position + transform.parent.forward * 8f + transform.parent.up * 1.4f;
+        else if (xMove == 0.0f && yMove == 0.0f)
             target = transform.parent.position - transform.parent.forward * 2.5f + transform.parent.up * 1.4f;
-        else {
+        else
+        {
             float magic = (Player.transform.position - transform.position).magnitude;
             magic = Mathf.Min(2, magic);
             target = transform.parent.position -
                      transform.parent.forward * 2f +
-                     transform.parent.right * 3 * xMove * magic+
+                     transform.parent.right * 3 * xMove * magic +
                      transform.parent.up * (yMove + 0.3f) * 1.4f * magic;
         }
         transform.position = Vector3.MoveTowards(transform.position, target, LookSpeed * Time.deltaTime);
