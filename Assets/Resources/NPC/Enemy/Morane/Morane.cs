23ee3416bcf875755e61281ed33840226655777c
Holger Borum <blaa.vogn@gmail.com>
2017-04-17 14:52:49 +0200

    Camera starts with small zoom-in

---
+++ Assets/Resources/NPC/Enemy/Morane/Morane.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Morane : MonoBehaviour
+{
+    private enum State {ATTACK};
+    private State state = State.ATTACK;
+    private PlayerFollowers playerFollowers;
+    private Vector3 target;
+
+    private float Speed = 10.0f;
+
+    void Start ()
+    {
+        playerFollowers = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerFollowers>();
+    }
+	
+	void Update ()
+    {
+        switch (state)
+        {
+            case State.ATTACK:
+                target = playerFollowers.GetTarget().position;
+                Vector3 newPosition = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
+                Vector3 movement = newPosition - transform.position;
+                //wiggle.wiggleSpeed = movement.magnitude * 50 + 10;
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, movement, 0.1f, 0));
+                transform.position += transform.forward * movement.magnitude;
+                break;
+        }
+
+
+	}
+}
