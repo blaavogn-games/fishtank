c4d76c3fd40e177a5e7b0f60ead0833524abcd12
Unknown <themunk@live.dk>
2017-04-18 10:49:02 +0200

    main menu

---
+++ Assets/scenes/Mathias/flicker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flicker : MonoBehaviour
+{
+
+    public float timeOnMin = 0.1f;
+    public float timeOnMax = 0.1f;
+    public float timeOffMin = 0.5f;
+    public float timeOffMax = 0.5f;
+    private float changeTime = 0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > changeTime)
+        {
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+            if (GetComponent<Light>().enabled)
+            {
+                changeTime = Time.time + Random.Range(timeOnMin,timeOnMax);
+            }
+            else
+            {
+                changeTime = Time.time + Random.Range(timeOffMin, timeOffMax);
+            }
+        }
+    }
+}
