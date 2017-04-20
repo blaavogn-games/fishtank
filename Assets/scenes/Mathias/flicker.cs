b3d3aafb78881945ae27b485cce200294e30ff19
Stepcore <taub@itu.dk>
2017-04-20 10:18:55 +0200

    Beta folder and flickering fixed

--- Assets/scenes/Mathias/flicker.cs
+++ Assets/scenes/Mathias/flicker.cs
@@ -4,29 +4,49 @@ using System.Collections;
 public class flicker : MonoBehaviour
 {
 
-    public float timeOnMin = 0.1f;
-    public float timeOnMax = 0.1f;
-    public float timeOffMin = 0.5f;
-    public float timeOffMax = 0.5f;
+    public float timeOnMin = 0.2f;
+    public float timeOnMax = 0.5f;
+    public float timeOffMin = 0.01f;
+    public float timeOffMax = 0.1f;
     private float changeTime = 0f;
-
+    Light[] lights = new Light[3];
 
 
     // Update is called once per frame
+    void Start()
+    {
+        for (int i = 0; i<transform.childCount; i++)
+        {
+            lights[i] = transform.GetChild(i).GetComponent<Light>();
+        }
+    }
     void Update()
     {
 
         if (Time.time > changeTime)
         {
-            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
-            if (GetComponent<Light>().enabled)
+            foreach (Light l in lights)
+            {
+                l.enabled = !l.enabled;
+
+                if (l.enabled)
+                {
+                    changeTime = Time.time + Random.Range(timeOnMin, timeOnMax);
+                }
+                else
+                {
+                    changeTime = Time.time + Random.Range(timeOffMin, timeOffMax);
+                }
+            }
+            /*GetComponentInChildren<Light>().enabled = !GetComponentInChildren<Light>().enabled;
+            if (GetComponentInChildren<Light>().enabled)
             {
                 changeTime = Time.time + Random.Range(timeOnMin,timeOnMax);
             }
             else
             {
                 changeTime = Time.time + Random.Range(timeOffMin, timeOffMax);
-            }
+            }*/
         }
     }
 }
