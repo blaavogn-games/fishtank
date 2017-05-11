d0fc9c5d2d6f4233ccccd276eb9c933dca6c5215
Stepcore <taub@itu.dk>
2017-05-11 14:11:26 +0200

    Ending sequence in level 1

---
+++ Assets/RollingStone.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingStone : MonoBehaviour
+{
+    public GameObject FishNet;
+    public Rigidbody FinalWeed;
+    public HingeJoint[] joints;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject != FishNet)
+            return;
+        FinalWeed.isKinematic = false;
+        foreach (HingeJoint j in joints)
+        {
+            j.breakForce = 0;
+        }
+    }
+
+}
